4936f622641616b90f38cce5ab831d281a9a670e
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2019-07-07 17:19:27 +0200

    Almost PEP8

--- Connector_USB.3dshapes/USB_C_Receptacle_Palconn_UTC16-G.py
+++ Connector_USB.3dshapes/USB_C_Receptacle_Palconn_UTC16-G.py
@@ -1,95 +1,112 @@
 # Generated with CadQuery2
-# Boolean and align with kicad stepup tools with offsets:
-#   Y: -1.17
-#   X: 1.65
+# Coloring and checks done in kicad steup
+# No offsets
 
 import cadquery as cq
+from os.path import basename, splitext
 
-dims_body = {
-    "width" : 8.94,
-    "depth" : 7.32,
-    "height" : 3.25,
-    "radius" : 1.25,
-    "wall_thickness" : 0.345,
-    "cavity_depth" : 4.8
-}
+dims = {
+    "body": {
+        "width": 8.94,
+        "depth": 7.32,
+        "height": 3.25,
+        "radius": 1.25,
+        "wall_thickness": 0.345,
+        "cavity_depth": 4.8,
+        "offset": (0, -1.17, 0)
+    },
+    "tounge": {
+        "width": 6.69,
+        "height": 0.7,
+        "depth": 4.45,
+        "tip_chamfer": 0.5
+    },
+    "pegs": {
+        "diameter": 0.5,
+        "length": 0.76,
+        "tip_chamfer": 0.1,
+        "centers": [
+            (5.78/2, -2.63),
+            (-5.78/2, -2.63)
+        ]
+    },
+    "pins": {
+        "width": 0.2,
+        "length": 7.63 - 7.32,
+        "height": 0.15,
+        "centers": [
+            (-6.7/2, 0),
+            (-6.1/2, 0),
+            (-5.1/2, 0),
+            (-4.5/2, 0)
+        ]
+    },
+    "shield": {
+        "thickness": 0.3,
+        "length": 1,
+        "width": (0.8, 1.1),  # (front, back)
+        "centers_front": [
+            (-8.64/2, 1.06),
+            (8.64/2, 1.06)
+        ],
+        "centers_back": [
+            (8.64/2, -3.11),
+            (-8.64/2, -3.11)
+        ]
+    }
 
-dims_tounge = {
-    "width" : 6.69,
-    "height" : 0.7,
-    "depth" : 4.45,
-    "tip_chamfer" : 0.5
- }
-
-dims_pegs = {
-    "diameter" : 0.5,
-    "length" : 0.76,
-    "tip_chamfer" : 0.1,
-    "centers" : [
-        (5.78/2, -2.63),
-        (-5.78/2, -2.63)
-    ]
-}
-
-dims_pins = {
-    "width" : 0.2,
-    "length" : 7.63 - 7.32,
-    "height" : 0.15,
-    "centers" : [
-        (-6.7/2, 0),
-        (-6.1/2, 0),
-        (-5.1/2, 0),
-        (-4.5/2, 0)
-    ]
-}
-
-dims_shield = {
-    "thickness" : 0.3,
-    "length" : 1,
-    "width" : (0.8, 1.1), # (front, back)
-    "centers_front" : [
-        (-8.64/2, 1.06),
-        (8.64/2, 1.06)
-    ],
-    "centers_back" : [
-        (8.64/2, -3.11),
-        (-8.64/2, -3.11)
-    ]
 }
 
 x = -3.5/2
 while x < 0:
-    dims_pins["centers"].append((x, 0))
-    x = dims_pins["centers"][-1][0] + 0.5
+    dims["pins"]["centers"].append((x, 0))
+    x = dims["pins"]["centers"][-1][0] + 0.5
 
 for i in range(8):
-    dims_pins["centers"].append((dims_pins["centers"][i][0] * -1, 0))
+    dims["pins"]["centers"].append((dims["pins"]["centers"][i][0] * -1, 0))
 
 
-body = cq.Workplane("XZ").box(dims_body["width"], dims_body["height"], dims_body["depth"])\
-    .edges("|Y").fillet(dims_body["radius"]).faces("<Y").shell(-dims_body["wall_thickness"])
+body = cq.Workplane("XZ")\
+    .box(
+         dims["body"]["width"],
+         dims["body"]["height"],
+         dims["body"]["depth"],
+         centered=(True, False, True))\
+    .edges("|Y").fillet(dims["body"]["radius"])\
+    .faces("<Y").shell(-dims["body"]["wall_thickness"])\
+    .translate(dims["body"]["offset"])
 
 body_back = body.faces("<Y[-2]").workplane()\
-    .box(\
-         dims_body["width"]\
-         , dims_body["height"]\
-         , dims_body["depth"] - dims_body["cavity_depth"] - dims_body["wall_thickness"]\
-         , centered = (True, True, False)\
-         , combine = False)\
-    .edges("|Y").fillet(dims_body["radius"])
+    .box(
+         dims["body"]["width"],
+         dims["body"]["height"],
+         dims["body"]["depth"] - dims["body"]["cavity_depth"] - dims["body"]["wall_thickness"],
+         centered=(True, True, False),
+         combine=False)\
+    .edges("|Y").fillet(dims["body"]["radius"])
 
-shield_pins_front = cq.Workplane("XY").workplane(invert=True).pushPoints(dims_shield["centers_front"])\
-    .box(dims_shield["thickness"],dims_shield["width"][0],dims_shield["length"] + dims_body["height"]/2\
-         , centered = (True, True, False)\
-         , combine = True)\
-    .edges("|X and <Z").fillet(dims_shield["width"][0]/2 - 0.01)
+shield_pins_front = body.faces("<Z")\
+    .workplane(offset=- dims["body"]["height"]/2)\
+    .pushPoints(dims["shield"]["centers_front"])\
+    .box(
+        dims["shield"]["thickness"],
+        dims["shield"]["width"][0],
+        dims["shield"]["length"] + dims["body"]["height"]/2,
+        centered=(True, True, False),
+        combine=True)\
+    .edges("|X and <Z").fillet(dims["shield"]["width"][0]/2 - 0.01)
     
 
-shield_pins_back = cq.Workplane("XY").workplane(invert=True).pushPoints(dims_shield["centers_back"])\
-    .box(dims_shield["thickness"],dims_shield["width"][1],dims_shield["length"] + dims_body["height"]/2\
-         , centered = (True, True, False)\
-         , combine = True)\
-    .edges("|X and <Z").fillet(dims_shield["width"][1]/2 - 0.01)
+shield_pins_back = body.faces("<Z")\
+    .workplane(offset=- dims["body"]["height"]/2)\
+    .pushPoints(dims["shield"]["centers_back"])\
+    .box(
+        dims["shield"]["thickness"],
+        dims["shield"]["width"][1],
+        dims["shield"]["length"] + dims["body"]["height"]/2,
+        centered=(True, True, False),
+        combine=True)\
+    .edges("|X and <Z").fillet(dims["shield"]["width"][1]/2 - 0.01)
 
 body = body.union(body_back).union(shield_pins_back).union(shield_pins_front)
 del shield_pins_front
@@ -97,23 +114,37 @@ del shield_pins_back
 del body_back
 
 tounge = body.faces(">Y[-3]")\
-    .box(dims_tounge["width"],dims_tounge["height"],dims_tounge["depth"]\
-         , centered = (True, True, False)\
-         , combine = False)\
-    .edges("|Z and <Y").chamfer(dims_tounge["tip_chamfer"])
+    .box(
+        dims["tounge"]["width"],
+        dims["tounge"]["height"],
+        dims["tounge"]["depth"],
+        centered=(True, True, False),
+        combine=False)\
+    .edges("|Z and <Y").chamfer(dims["tounge"]["tip_chamfer"])
 
-pegs =body.faces("<Z[-3]").workplane().pushPoints(dims_pegs["centers"])\
-    .circle(dims_pegs["diameter"]/2).extrude(dims_pegs["length"],combine = False)\
-    .edges("<Z").chamfer(dims_pegs["tip_chamfer"])
+pegs = body.faces("<Z[-2]").workplane().pushPoints(dims["pegs"]["centers"])\
+    .circle(dims["pegs"]["diameter"]/2)\
+    .extrude(dims["pegs"]["length"], combine=False)\
+    .edges("<Z").chamfer(dims["pegs"]["tip_chamfer"])
 
-pins = body.faces(">Y").workplane().center(0, -dims_body["height"]/2)\
-    .pushPoints(dims_pins["centers"])\
-    .box(dims_pins["width"],dims_pins["height"],dims_pins["length"]\
-         , centered = (True, False, False)\
-         , combine = False)\
+pins = body.faces(">Y").workplane().center(0, -dims["body"]["height"]/2)\
+    .pushPoints(dims["pins"]["centers"])\
+    .box(
+        dims["pins"]["width"],
+        dims["pins"]["height"],
+        dims["pins"]["length"],
+        centered=(True, False, False),
+        combine=False)\
 
+combined = body.union(tounge).union(pegs).union(pins)
 
-show_object(body, name="body", options = {"color" : "#c8c8c8"})
-show_object(tounge, name="tounge", options = {"color" : "#676767"})
-show_object(pegs, name="pegs", options = {"color" : "#676767"})
-show_object(pins, name="pins", options = {"color" : "#d9bd2d"})
+# If this is being run from the commandline,
+# export a step named the same as the script
+if '__file__' in globals():
+    with open("{}.step".format(splitext(basename(__file__))[0]), "w+") as f:
+        cq.exporters.exportShape(combined, cq.exporters.ExportTypes.STEP, f)
+else:
+    show_object(body, name="body", options = {"color" : "#c8c8c8"})
+    show_object(tounge, name="tounge", options = {"color" : "#676767"})
+    show_object(pegs, name="pegs", options = {"color" : "#676767"})
+    show_object(pins, name="pins", options = {"color" : "#d9bd2d"})
